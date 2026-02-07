e3551d310e5084ba1135a5cd5d6d6064ba1cd171
gmccannon <gwm13@uakron.edu>
2026-02-06 21:55:00 -0500

    Adds stuff

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QFileDialog>
+#include <taglib/fileref.h>
+#include <taglib/tag.h>
+#include <taglib/audioproperties.h>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,10 +23,47 @@ void MainWindow::on_pushButton_clicked()
     QString fileName = QFileDialog::getOpenFileName(
         this,
         "Open File",
-        QDir::homePath(),              // starting directory
+        QDir::homePath(),
         "Music files (*.mp3 *.flac *.wav *.aac *.ogg *.m4a *.opus);;All files (*.*)"
-    );
+        );
 
     ui->filePathBox->setText(fileName);
+}
+
+
+
+void MainWindow::on_getMetadataButton_clicked()
+{
+    QString fileName = ui->filePathBox->text();
+
+    if (fileName.isEmpty())
+        return;
+
+    TagLib::FileRef f(fileName.toUtf8().constData());
+
+    if (!f.isNull() && f.tag()) {
+        TagLib::Tag *tag = f.tag();
+
+        QString title  = QString::fromStdString(tag->title().to8Bit(true));
+        QString artist = QString::fromStdString(tag->artist().to8Bit(true));
+        QString album  = QString::fromStdString(tag->album().to8Bit(true));
+        QString genre  = QString::fromStdString(tag->genre().to8Bit(true));
+        int year       = tag->year();
+        int track      = tag->track();
+
+        qDebug() << "Title:" << title;
+        qDebug() << "Artist:" << artist;
+        qDebug() << "Album:" << album;
+        qDebug() << "Genre:" << genre;
+        qDebug() << "Year:" << year;
+        qDebug() << "Track:" << track;
+    }
+
+    if (f.audioProperties()) {
+        TagLib::AudioProperties *props = f.audioProperties();
+        qDebug() << "Length (s):" << props->length();
+        qDebug() << "Bitrate:" << props->bitrate();
+        qDebug() << "Sample rate:" << props->sampleRate();
+    }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -20,6 +20,8 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_getMetadataButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
